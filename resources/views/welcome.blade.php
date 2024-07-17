8e3029a1864beb1148053feb9841ff4bd337a102
makuguren <markglenmiguel@outlook.com>
2024-07-17 18:04:54 +0800

    Fix dashboard for instructor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
                                 @auth('instructor')
                                     <a
-                                        href="{{ route('instructor.dashboard') }}"
+                                        href="{{ route('instructor.dashboard.index') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Instructor Dashboard
