e2e17368db5a979600833e4370b3491796091931
makuguren <markgmiguel@outlook.com>
2024-11-20 06:05:36 +0000

    Revised seat plan for students.

--- resources/views/livewire/faculty/seat-plan/seatstud.blade.php
+++ resources/views/livewire/faculty/seat-plan/seatstud.blade.php
@@ -11,18 +11,18 @@
 </head>
 <body class="font-body">
 
-    <header class="flex justify-between items-center p-6">
+    <header class="flex items-center justify-between p-6">
         <img src="{{ $cspcimageBase64 }}" alt="CSPC Logo" class="h-16">
         <img src="{{ $ccsimageBase64 }}" alt="CCS Logo" class="h-16">
     </header>
 
     <hr class="border-t-4 border-gray-300">
 
-    <div class="flex flex-col justify-center items-center mt-6">
+    <div class="flex flex-col items-center justify-center mt-6">
         <h1 class="text-2xl font-bold">STUDENT'S SEAT PLAN</h1>
-        <div class="text-1xl font-medium">SUBJECT: <span class="font-bold">{{ $subject ?? ' ' }}</span></div>
-        <div class="text-1xl font-medium">SECTION: <span class="font-bold">{{ $section ?? ' ' }}</span></div>
-        <div class="text-1xl font-medium">FACULTY: <span class="font-bold">{{ $faculty ?? ' ' }}</span></div>
+        <div class="font-medium text-1xl">SUBJECT: <span class="font-bold">{{ $subject ?? ' ' }}</span></div>
+        <div class="font-medium text-1xl">SECTION: <span class="font-bold">{{ $section ?? ' ' }}</span></div>
+        <div class="font-medium text-1xl">FACULTY: <span class="font-bold">{{ $faculty ?? ' ' }}</span></div>
     </div>
 
     <div class="flex items-center justify-center">
@@ -39,16 +39,16 @@
                         @if ($seat->seat_number === 0)
                             <div class="flex items-center justify-center" style="width: 40px; height: 40px; margin: 5px;" class="tooltip"></div>
                         @elseif (!isset($seatplansArray[$seat->seat_number]))
-                            <div id="{{ $seat->seat_number }}" class="flex items-center justify-center bg-blue-700 border border-black box font-medium" style="width: 40px; height: 40px; margin: 5px;">{{ $seat->seat_number }}</div>
+                            <div id="{{ $seat->seat_number }}" class="flex items-center justify-center font-medium bg-blue-700 border border-black box" style="width: 40px; height: 40px; margin: 5px;">{{ $seat->seat_number }}</div>
                         @else
-                            <div id="{{ $seat->seat_number }}" class="flex items-center justify-center bg-red-700 border border-black box font-medium" style="width: 40px; height: 40px; margin: 5px;">{{ $seat->seat_number }}</div>
+                            <div id="{{ $seat->seat_number }}" class="flex items-center justify-center font-medium bg-red-700 border border-black box" style="width: 40px; height: 40px; margin: 5px;">{{ $seat->seat_number }}</div>
                         @endif
                     @endforeach
                 </div>
             @endforeach
 
             <div class="flex justify-center col-span-2 mt-5">
-                <div class="flex items-center justify-center bg-blue-700 border border-black font-bold" style="width: 300px; height: 40px;">FACULTY TABLE</div>
+                <div class="flex items-center justify-center font-bold bg-blue-700 border border-black" style="width: 300px; height: 40px;">FACULTY TABLE</div>
             </div>
         </div>
     </div>
@@ -62,11 +62,11 @@
         $secondHalf = array_slice($seatplansArray, $halfSeats, null, true);
     @endphp
 
-    <div class="flex gap-20 justify-center">
+    <div class="flex gap-20 pl-10 pr-10">
         <div class="w-1/2">
             <table class="table table-zebra">
                 <thead>
-                    <tr class="bg-base-200 rounded-md">
+                    <tr class="rounded-md bg-base-200">
                         <th>SEAT</th>
                         <th>NAME</th>
                     </tr>
@@ -87,7 +87,7 @@
         <div class="w-1/2">
             <table class="table table-zebra">
                 <thead>
-                    <tr class="bg-base-200 rounded-md">
+                    <tr class="rounded-md bg-base-200">
                         <th>SEAT</th>
                         <th>NAME</th>
                     </tr>
